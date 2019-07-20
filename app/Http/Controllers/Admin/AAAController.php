3bdd8c14a109e9d3e2eedeb66655e86c5aa7d9fe
CHR1718 <kitaakita168@gmail.com>
2019-07-20 03:37:45 +0000

    PHP/Laravel 09 Routingについて理解する(課題)

---
+++ app/Http/Controllers/Admin/AAAController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AAAController extends Controller
+{
+    //
+     public function bbb()
+     {
+    //   
+     }
+}
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -29,3 +29,4 @@ class ProfileController extends Controller
     }
 }
 
+
