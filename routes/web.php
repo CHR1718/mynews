1385ab5b8683892925b30135af9407227bff75aa
CHR1718 <kitaakita168@gmail.com>
2019-08-18 03:21:27 +0000

    PHP Laravel 12 課題

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::get('XXX', 'Admin\AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 Auth::routes();
