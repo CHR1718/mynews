e5fc51ebb18aecfe59d86cb9f0edf0d22b8ed1b4
CHR1718 <kitaakita168@gmail.com>
2019-10-20 04:39:22 +0000

    PHP Laravel 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,10 +15,21 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
+    // public function create(Request $request)
+    // {
+    //     $this->validate($request, Profile::$rules);
+        
+    //     $profile = new Profile;
+    //     $form = $request->all();
+        
+    //     // フォームから送信されてきた_tokenを削除する
+    //     unset($form['_token']);
+        
+    //     $profile->fill($form);
+    //     $profile->save();
+        
+    //     return redirect('admin/profile/create');
+    // }
 
     public function edit()
     {
@@ -27,6 +40,52 @@ class ProfileController extends Controller
     {
         return redirect('admin/profile/edit');
     }
+    
+ 
+    public function create(Request $request)
+  {
+
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+    //   // formに画像があれば、保存する
+    //   if ($form['image']) {
+    //     $path = $request->file('image')->store('public/image');
+    //     $profile->image_path = basename($path);
+    //   } else {
+    //       $profile->image_path = null;
+    //   }
+
+      unset($form['_token']);
+      unset($form['image']);
+    
+    $profile->fill($form);
+    $profile->save();
+
+      return redirect('admin/profile/create');
+  }
+
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
+ public function delete(Request $request)
+  {
+      // 該当するProfile Modelを取得
+      $news = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  }  
+
 }
-
-
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -18,7 +18,7 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="name">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="neme" value="{{ old('name') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     <div class="form-group row">
@@ -36,7 +36,7 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="introduction">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     {{ csrf_field() }}
